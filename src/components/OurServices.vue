6c4919ef0c00b8b518fa8937b484188fc9df46ae
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-07-01 01:07:30 +0200

    Added ServicesView

---
+++ src/components/OurServices.vue
@@ -0,0 +1,86 @@
+<template>
+<link rel="stylesheet" href="https://unpkg.com/bs-brain/bsb.css" />
+ <section class="bg-light py-5 py-xl-8">
+  <div class="container mb-5 mb-md-6">
+    <div class="row justify-content-md-center">
+      <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 text-center">
+        <h2 class="mb-4 display-5">Our Services</h2>
+        <p class="text-secondary mb-4 mb-md-5">Our club is dedicated to promoting the sport of judo and fostering a supportive and inclusive environment for our members. Here are some of our key services and areas of expertise:</p>
+        <hr class="w-50 mx-auto mb-0 text-secondary">
+      </div>
+    </div>
+  </div>
+
+  <div class="container overflow-hidden">
+    <div class="row gy-2 g-md-2 g-xl-3 bsb-project-2-grid">
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale-up bsb-hover-scale" loading="lazy" src="https://www.judoinside.com/photos/hans/2018/Mediterranean_Games_Tarragona/2018_tarragona_akil_gjakova.jpg" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInLeft">Judo Training</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src="https://sweetscienceoffighting.com/wp-content/uploads/2020/10/Conditioning-requirements-for-Judo-1024x683.jpg" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInUp">Competition Preparation</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale-up bsb-hover-scale" loading="lazy" src="https://breakingmuscle.com/wp-content/uploads/2012/10/uchimata.jpg" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInLeft">Youth Development</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src="https://www.kreedon.com/wp-content/uploads/2021/08/istockphoto-621819278-612x612-1.jpg" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInUp">Sportsmanship</h3>
+            <div class="text-white bsb-hover-fadeInDown">Values</div>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src="https://img.olympicchannel.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/v1541870548/primary/npmz7jqva6f36wc6vo2t" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInUp">Fitness and Conditioning</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="col-12 col-md-4 bsb-project-2-item">
+        <figure class="rounded rounded-3 overflow-hidden bsb-hover-overlay m-0">
+          <a href="#!">
+            <img class="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src="https://kljudo.com/content/images/2021/12/Judo.JPG" alt="">
+          </a>
+          <figcaption>
+            <h3 class="text-white bsb-hover-fadeInUp">Self-Defense Skills</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+    </div>
+  </div>
+</section>
+</template>
