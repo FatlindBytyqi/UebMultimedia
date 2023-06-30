207dc66caf939adc80800a6e5562d29e478a3917
FatlindBytyqi <95626338+FatlindBytyqi@users.noreply.github.com>
2023-06-30 22:06:41 +0200

    Fixed some Landing Page settings

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,7 +2,7 @@
     <!-- CSS Files -->
 <link rel="stylesheet" href="https://unpkg.com/bs-brain/bsb.css" />
     <!-- Call To Action 1 - Bootstrap Brain Component -->
-<section class="px-2 py-8 py-xxl-16 background-position-center background-size-cover background-attachment-fixed bsb-overlay" style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Downing_College_Cambridge_%2817869693574%29.png/640px-Downing_College_Cambridge_%2817869693574%29.png');">
+<section class="px-2 py-8 py-xxl-16 background-position-center background-size-cover background-attachment-fixed bsb-overlay" style="background-image: url('https://www.leparisien.fr/resizer/GngA8tJnu0yArs6-dYqAhB1WbT8=/arc-anglerfish-eu-central-1-prod-leparisien/public/MK63RBQRENH6HDA3W2GTTT2SYE.jpg');">
   <div class="container">
     <div class="row">
       <div class="col-12 col-md-9 col-lg-8 col-xl-7 col-xxl-6">
